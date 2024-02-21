c253d6f65f9fe6d1b34b96f4d61ba4467b9b80a2
Ravin Paul <rpr@gmail.com>
2024-02-21 20:29:48 +0530

    Display Critic Score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,19 @@
+import { Badge } from "@chakra-ui/react";
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge
+      fontSize="14px"
+      colorScheme={color}
+      borderRadius={"4px"}
+      paddingX={2}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
