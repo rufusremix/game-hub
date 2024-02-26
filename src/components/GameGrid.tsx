c16975bfcf05767a38e99fa9075a108d02cfe5fc
Rufus C <rprc@gmail.com>
2024-02-26 11:36:17 +0530

    Displaying the Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,11 +10,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <p>{error}</p>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding="10px" spacing={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
